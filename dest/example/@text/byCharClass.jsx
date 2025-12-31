16a4b54e10a5ab4feec04cf3452fdb49b4ffd398
kareobana <kareobana@atarabi.com>
2025-12-31 14:46:11 +0900

    add fontFamily generator

--- dest/example/@text/byCharClass.jsx
+++ dest/example/@text/byCharClass.jsx
@@ -3,4 +3,5 @@ TextStyle.byCharClass()
     .rule(CharClass.Hiragana, { fillColor: [1, 0, 0], fontSize: 50 })
     .rule([CharClass.Katakana, CharClass.Yakumono], { fillColor: [0, 1, 0], fontSize: 40 })
     .rule(CharClass.Kanji, { fillColor: [0, 0, 1], fontSize: 70 })
+    .rule(CharClass.Latin, { applyFill: false, applyStroke: true, strokeColor: [1, 1, 0] })
     .apply();
